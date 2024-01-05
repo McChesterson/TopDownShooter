53909ee8362f60cbc3a96f4ea8e91916c7f74249
McChesterson <114367559+McChesterson@users.noreply.github.com>
2024-01-05 15:20:36 -0800

    added more stuff

--- Top Down Shooter/Assets/Scripts/ProjectV3/PlayerV3Movement.cs
+++ Top Down Shooter/Assets/Scripts/ProjectV3/PlayerV3Movement.cs
@@ -12,6 +12,10 @@ public class PlayerV3Movement : MonoBehaviour
     [Space]
     [SerializeField] Vector2 movement;
     public float playerSpeed;
+    public float rotateSpeed = 5;
+
+    Quaternion playerDirection = Quaternion.identity;
+    bool rotateMode = false;
 
     void Start()
     {
@@ -19,16 +23,71 @@ public class PlayerV3Movement : MonoBehaviour
     }
     void Update()
     {
+        //rb.rotation = 0;
+        rb.angularVelocity = 0;
+        transform.rotation = playerDirection;
+        if (Input.GetKeyDown(isPlayer1? KeyCode.LeftShift : KeyCode.RightShift) && !rotateMode)
+        {
+            rotateMode = true;
+            Debug.Log(gameObject.name + " rotateMode = " + rotateMode);
+        }
+        else if (Input.GetKeyDown(isPlayer1 ? KeyCode.LeftShift : KeyCode.RightShift))
+        {
+            rotateMode = false;
+            Debug.Log(gameObject.name + " rotateMode = " + rotateMode);
+        }
+        if (rotateMode)
+        {
+            //rotating the player
+            float zRot;
+            zRot = transform.rotation.z;
+            transform.Rotate(0, 0, zRot -= Input.GetAxisRaw(isPlayer1? "HorizontalWASD" : "HorizontalArrows") * rotateSpeed);
+            Debug.Log(zRot);
+            return;
+        }
+        else
+        {
+            //moving the player regularly
+            movement.x = Input.GetAxisRaw(isPlayer1? "HorizontalWASD" : "HorizontalArrows");
+            movement.y = Input.GetAxisRaw(isPlayer1? "VerticalWASD" : "VerticalArrows");
+            movement = movement.normalized;
+        }
+        /*
+        //all of the controls for Player1
         if (isPlayer1)
         {
-            movement.x = Input.GetAxisRaw("HorizontalWASD");
-            movement.y = Input.GetAxisRaw("VerticalWASD");
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !rotateMode)
+            {
+                rotateMode = true;
+                Debug.Log("Player1's rotateMode = " + rotateMode);
+                //transform.Rotate(0, 0, rotateSpeed);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                rotateMode = false;
+                Debug.Log("Player1's rotateMode = " + rotateMode);
+            }
+            
         }
-        if (!isPlayer1)
+        //all of the controls for Player2
+        else
         {
+            if (Input.GetKeyDown(KeyCode.RightShift) && !rotateMode)
+            {
+                rotateMode = true;
+                Debug.Log("Player2's rotateMode = " + rotateMode);
+                //transform.Rotate(0, 0, rotateSpeed);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                rotateMode = false;
+                Debug.Log("Player2's rotateMode = " + rotateMode);
+            }
             movement.x = Input.GetAxisRaw("HorizontalArrows");
             movement.y = Input.GetAxisRaw("VerticalArrows");
+            movement = movement.normalized;
         }
+        */
     }
     private void FixedUpdate()
     {
